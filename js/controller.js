db92ff2dd3cdd88eeab91c8244448e7000727385
wkwyatt <wkwyatt1@gmail.com>
2015-12-01 10:38:52 -0500

    added states to array corresponding with color

--- js/controller.js
+++ js/controller.js
@@ -17,6 +17,9 @@ stateMap.directive('clickState', function(){
 
 function interactiveMapCntrl($scope){
 	$scope.states = states;
+	$scope.redStatesTotal = redStates.length;
+	$scope.blueStatesTotal = blueStates.length;
+	$scope.openStatesTotal = openStates.length;
 }
 
 function getNewColor(state) {
@@ -25,10 +28,23 @@ function getNewColor(state) {
 	// if state color equals open change to red
 	console.log(state);
 	if (state.stateColor == "red") {
+		// add element to appropriate array and remove from old array
+		redStates.splice(redStates[redStates.indexOf(state)], 1);
+		blueStates[state.id] = state;
+		console.log("redstates:");
+		console.log(redStates);
+		console.log("bluestates:");
+		console.log(blueStates);
 		return "blue";
 	} else if (state.stateColor == "blue") {
+		// add element to appropriate array and remove from old array
+		blueStates.splice(blueStates[blueStates.indexOf(state)], 1);
+		openStates[state.id] = state;
 		return "open";
 	} else if (state.stateColor == "open") {
+		// add element to appropriate array and remove from old array
+		openStates.splice(openStates[openStates.indexOf(state)], 1);
+		redStates[state.id] = state;
 		return "red";
 	} else  {
 		return console.log("error");
